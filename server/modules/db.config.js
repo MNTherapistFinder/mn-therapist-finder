206f821ad247658197085a4ed335ea108b8d3c11
Christopher Black <chris@primeacademy.io>
2017-11-16 13:34:29 -0600

    Changed user factory to a service.

--- server/modules/db.config.js
+++ server/modules/db.config.js
@@ -2,27 +2,29 @@ var mongoose = require('mongoose');
 
 // Mongo Connection //
 var mongoURI = '';
-// process.env.MONGODB_URI will only be defined if you are running on Heroku
-if(process.env.MONGODB_URI != undefined) {
+// process.env.MONGODB_URI will only be defined if you
+// are running on Heroku
+if (process.env.MONGODB_URI != undefined) {
     // use the string value of the environment variable
     mongoURI = process.env.MONGODB_URI;
 } else {
     // use the local database server
-    mongoURI = 'mongodb://localhost:27017/solo';
+    mongoURI = 'mongodb://localhost:27017/soloproject';
 }
 
-// var mongoURI = "mongodb://localhost:27017/passport";
-var mongoDB = mongoose.connect(mongoURI).connection;
-
-mongoDB.on('error', function(err){
-   if(err) {
-     console.log("MONGO ERROR: ", err);
-   }
-   res.sendStatus(500);
+mongoose.connect(mongoURI, {
+    useMongoClient: true
 });
 
-mongoDB.once('open', function(){
-   console.log("Connected to Mongo!");
+mongoose.connection.on('error', function (err) {
+    if (err) {
+        console.log("MONGO ERROR: ", err);
+    }
+    res.sendStatus(500);
 });
 
-module.exports = mongoDB;
+mongoose.connection.on('open', function () {
+    console.log("Connected to Mongo!");
+});
+
+module.exports = mongoose;
