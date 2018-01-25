79e86cb389d36681a83409dfe43f602710b0e029
Stephanie Ratanas <stephanieratanas@gmail.com>
2018-01-25 14:09:37 -0600

    add three input boxes on home page and get request for zip codes

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -2,7 +2,7 @@ myApp.controller('HomeController', function(HomeService) {
     console.log('HomeController created');
     var vm = this;
 
-
+    vm.searchZipCodes = HomeService.searchZipCodes;
 
 
   });
--- server/public/scripts/services/home.service.js
+++ server/public/scripts/services/home.service.js
@@ -2,8 +2,20 @@ myApp.service('HomeService', function($http, $location){
     console.log('HomeService Loaded');
     var self = this;
 
-
-
-
+//GET request for zip code search
+self.searchZipCodes = function(){
+  console.log('searchZipCodes clicked');
+  
+    $http({
+      method: 'GET',
+      url: '/zipCodes'
+    }).then(function (response) {
+      console.log('response from searchZipCodes', response);
+      self.zipCodes = response.data;
+ 
   });
+}
+
+
+});
   
