d32f1fc9aaa16c9223acf7d4d4e0d1f6e9753d0c
liz elton <eeeqqqeee@gmail.com>
2018-01-26 16:13:26 -0600

    first half of directory, fix get req

--- server/public/scripts/services/directory.service.js
+++ server/public/scripts/services/directory.service.js
@@ -1,38 +1,20 @@
-myApp.service('DirectoryService', function($http, $location){
-    console.log('DirectoryService Loaded');
-    var self = this;
+myApp.service('DirectoryService', function ($http, $location) {
+  console.log('DirectoryService Loaded');
+  var self = this;
 
-self.therapistInfo = {list:[]}
-self.therapistLicense = {list:[]}
+  self.therapistInfo = { list: [] }
 
+  // GET request for therapist information on main directory page
+  self.getTherapistInfo = function () {
+    console.log('in getTherapistInfo');
 
-//GET request for therapist information on main directory page
-self.getTherapistInfo = function(){
-  console.log('in getTherapistInfo');
-  
     $http({
       method: 'GET',
       url: '/directory/therapistinfo'
     }).then(function (response) {
       console.log('response from getTherapistInfo', response);
       self.therapistInfo.list = response.data;
- 
-  });
-}
+    });
+  } // end GET request for therapist information on main directory page
 
-self.getTherapistLicenseType = function(){
-  console.log('in getTherapistLicenseType');
-
-    $http({
-      method: 'GET',
-      url: '/directory/therapistlicensetype'
-    }).then(function (response) {
-      console.log('response from getTherapistLicenseType', response);
-      self.therapistLicense.list = response.data;
- 
-  });
-}
-
-
-
-  });
+});
