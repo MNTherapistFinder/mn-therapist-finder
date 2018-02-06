ac8bd1be08b94766d19bf5246b67dedc3d66ef16
gaglianojoseph <gagli012@umn.edu>
2018-02-06 10:02:28 -0600

    start search view buildout

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,8 +1,28 @@
-myApp.controller('SearchController', function(HomeService) {
+myApp.controller('SearchController', ['$mdDialog','$mdSidenav','$mdMedia','HomeService', function ( $mdDialog, $mdSidenav, $mdMedia, HomeService) {
     console.log('InfoController created');
     var vm = this;
     vm.message = 'hey'
-    vm.searchResults = HomeService.searchResults
+    vm.searchResults = HomeService.searchResults;
 
-  });
-  
+    vm.openLeftMenu = function () {
+        $mdSidenav('left').toggle();
+    };
+
+    vm.showTherapistInfo = function(event) {
+        console.log('showTherapistInfo clicked');
+        $mdDialog.show({
+          parent: angular.element(document.body),
+          templateUrl: '/views/partials/therapist.modal.html',
+          controller: 'ModalController as mc',
+          targetEvent: event,
+          clickOutsideToClose:true,
+          fullscreen: self.customFullscreen,
+          locals: {
+            modalData:{
+              event: event
+            }
+          }
+    
+        })};
+
+}]);
