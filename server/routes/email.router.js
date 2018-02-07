b6a0447e8fca8d1a042d7b6a904a805a6611c51a
Stephanie Ratanas <stephanieratanas@gmail.com>
2018-02-07 15:36:23 -0600

    fix bug in appointment view, styled appt request modal

--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -7,7 +7,7 @@ var smtpTransport = nodemailer.createTransport({
     service: "Gmail",
     auth: {
         user: "mntherapistfinder@gmail.com",
-        pass: "wordofmouth"
+        pass: ""
     }
 });
 var rand,mailOptions,host,link;
